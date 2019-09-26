fd5058534fc57509903f3e1393001a4aae32c723
taslimy <taslimer@gmail.com>
2019-09-25 23:20:09 -0400

    add coords
    adding coords
    
    Co-Authored-By: lflores12 <lflores12@users.noreply.github.com>
    Co-Authored-By: ba-zen <ba-zen@users.noreply.github.com>

--- adventure/api.py
+++ adventure/api.py
@@ -38,7 +38,9 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players}, safe=True)
+    x = room.x
+    y = room.y
+    return JsonResponse({'uuid': uuid, 'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'x': room.x, 'y': room.y }, safe=True)
 
 
 # @csrf_exempt
@@ -51,7 +53,11 @@ def move(request):
     player_uuid = player.uuid
     data = json.loads(request.body)
     direction = data['direction']
+
     room = player.room()
+  
+    x = room.x
+    y = room.y
     nextRoomID = None
     if direction == "n":
         nextRoomID = room.n_to
@@ -72,12 +78,12 @@ def move(request):
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
         # for p_uuid in nextPlayerUUIDs:
         #     pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name': player.user.username, 'title': nextRoom.title, 'description': nextRoom.description, 'players': players, 'error_msg': ""}, safe=True)
+        return JsonResponse({'name': player.user.username, 'title': nextRoom.title, 'description': nextRoom.description, 'players': players, 'error_msg': "", 'x': room.x, 'y': room.y}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'error_msg': "You cannot move that way."}, safe=True)
+        return JsonResponse({'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'error_msg': "You cannot move that way.", 'x': room.x, 'y': room.y}, safe=True)
 
-
+# 'x': room.x, 'y': room.y
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
